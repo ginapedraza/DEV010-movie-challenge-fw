8fe139cdbc8a290ae0a7f129be22d6e0b4b95260
unknown <ginapedraza00@gmail.com>
2023-11-07 11:14:52 -0300

    test historia 3 mejorados, 80% en branches

--- src/components/MoviePage/MoviePage.test.tsx
+++ src/components/MoviePage/MoviePage.test.tsx
@@ -38,4 +38,50 @@ describe('MoviePage Component', () => {
     expect(screen.getByText("Fearless alpine climbers Ueli Steck and Dani Arnold enter into a death-defying rivalry to set speed records on the Swiss Alps' great north faces.")).toBeInTheDocument();
     expect(screen.getByAltText('Duell am Abgrund')).toBeInTheDocument();
   });
+
+  it('renders "No Overview provided" when movie overview is empty', async () => {
+    const mockResponse = {
+      id: 1174725,
+      original_title: 'Duell am Abgrund',
+      vote_average: 6.456,
+      release_date: '2023-10-04',
+      vote_count: 34,
+      overview: '', // Cadena vacía
+      poster_path: '/3H8u03qr9se2pCv3QTHR1gxSa9k.jpg',
+    };
+  
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: async () => mockResponse,
+    } as Response);
+  
+    await act(async () => {
+      render(
+        <MemoryRouter initialEntries={['/movie/123']}>
+          <Routes>
+            <Route path="/movie/:movieId" element={<MoviePage />} />
+          </Routes>
+        </MemoryRouter>
+      );
+    });
+  
+    expect(screen.getByText('No Overview provided')).toBeInTheDocument();
+  
+    // Asegúrate de restaurar global.fetch después de la prueba
+    jest.restoreAllMocks();
+  });
+  /*it.only('handles error and logs it', async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      throw new Error('Network error');
+    });
+  
+    await render(
+      <MemoryRouter initialEntries={['/movie/123']}>
+        <Routes>
+          <Route path="/movie/:movieId" element={<MoviePage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+  
+    expect(console.error).toHaveBeenCalledWith('Error:', expect.any(Error));
+  });*/
 });
