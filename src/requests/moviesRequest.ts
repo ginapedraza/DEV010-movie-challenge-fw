fd96480fd8b8a9f4723de26fb82c7b2c4f765b45
unknown <ginapedraza00@gmail.com>
2023-11-10 16:43:18 -0300

    project feedback listo

--- src/requests/moviesRequest.ts
+++ src/requests/moviesRequest.ts
@@ -22,21 +22,21 @@ export type ApiResponse = {
 //Guardamos en una constate la Api Key que necesitaremos como autorizavión
 export const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGUzMGMxZGY5ODEyZjAwZjFjOTZjMGNjYzI2OGZmZSIsInN1YiI6IjY1Mjk0Zjk1NjI5YjJjMDBjNTllNjM3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YcDVFDVV7B2k1qCjx1JHzsKXiew57qQD58OE03_0bmk'; 
 
-export const buildMovieApiUrl = (page: number, filterByValue: string, sortByValue: string) => {
-    return `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&${filterByValue}&sort_by=${sortByValue}&with_genres=99`;
+export const buildMovieApiUrl = (currentPage: number, filterByValue: string, sortByValue: string) => {
+    return `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${currentPage}&${filterByValue}&sort_by=${sortByValue}&with_genres=99`;
   }
 
 //Creamos una promesa para hacer la solicitud HTTP con fetch y obtenerla manejando los errores
 export const requestMovies = (url: string, options: RequestInit): Promise<ApiResponse>=> {
   // Utiliza la función fetch para realizar una solicitud HTTP con la URL y las opciones proporcionadas.
   return fetch(url, options)
-    .then(async (res) => {
+    .then(async (response) => {
       
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
       // Si la respuesta es exitosa, convierte el cuerpo de la respuesta a formato JSON (objeto Javascript) para ser leído por la aplicación
-            const data = await res.json();
+            const data = await response.json();
       //Indicamos que la data es la ApiResponse de las peliculas
       return data as ApiResponse;
     })
